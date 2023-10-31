06f9338d33a82264bfb25da1e3463c7604ae665b
zeicksoule <102954378+zeicksoule@users.noreply.github.com>
2023-10-31 19:55:33 +0100

    Delete src/shared/state/Cards.cpp

--- src/shared/state/Cards.cpp
+++
@@ -1,8 +0,0 @@
-#include "Cards.h"
-#include <iostream>
-using namespace std;
-using namespace state; 
-
-Cards::Cards(int idCard, std::string name, std::string description, CardTypes type, int nombresDeCartes) {
-
-}
