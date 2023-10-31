bfceec17b8ec17f5a021f5785c6ab0a9c59baa55
zeicksoule <102954378+zeicksoule@users.noreply.github.com>
2023-10-31 21:15:20 +0100

    Add files via upload

---
+++ src/shared/state/Monsters.cpp
@@ -0,0 +1,24 @@
+#include "Monsters.h"
+#include <iostream>
+
+using namespace std;
+using namespace state;
+
+Monsters::Monsters(int effet, int level, int attack, int defense, bool position) {
+    this->effet = effet;
+    this->level = level;
+    this->attack = attack;
+    this->defense = defense;
+    this->position = position;
+}
+void Monsters::attackCard(Cards card) {
+    cout << "attackCard\n";
+}
+
+void Monsters::attackPlayer(Players player) {
+    cout << "attackPlayer\n";
+}
+
+void Monsters::setSpell() {
+    cout << "attack\n";
+}
