dfd7fe57f27de20e0d4f82588783927741b902c9
zeicksoule <102954378+zeicksoule@users.noreply.github.com>
2023-10-31 19:34:47 +0100

    Add files via upload

--- src/shared/state/Cards.cpp
+++ src/shared/state/Cards.cpp
@@ -1,6 +1,7 @@
 #include "Cards.h"
 #include <iostream>
 using namespace std;
+using namespace state; 
 
 Cards::Cards(int idCard, std::string name, std::string description, CardTypes type, int nombresDeCartes) {
     this->idCard = idCard;
