214f25ff4ce1e1839211cc2602294574015cb7ae
zeicksoule <102954378+zeicksoule@users.noreply.github.com>
2023-10-31 20:01:36 +0100

    Add files via upload

---
+++ src/shared/state/Cards.cpp
@@ -0,0 +1,10 @@
+#include "Cards.h"
+#include "CardTypes.h"
+#include <iostream>
+
+using namespace std;
+using namespace state; 
+
+Cards::Cards(int idCard, std::string name, std::string description, CardTypes typeCarte, int nombresDeCartes) {
+
+}
